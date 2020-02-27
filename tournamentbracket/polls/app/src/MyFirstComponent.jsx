d8e6dad080b88fcf8e4f8245b3543e93928caca6
Daniel <danielzrust@gmail.com>
2020-02-26 23:37:30 -0700

    Removed bundle js and fixed up broken stuff

---
+++ tournamentbracket/polls/app/src/MyFirstComponent.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const MyFirstComponent = () => (
+    <div>
+        <h2>
+            Hello World
+        </h2>
+        <div>
+            This is my first webpage!!!!!!
+            "\n"
+            {"\n"}
+            <br />
+            I'm gonna do stuff with it. Yay!
+        </div>
+    </div>
+);
+
+export default MyFirstComponent;
