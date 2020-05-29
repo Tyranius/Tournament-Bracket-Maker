f290404bcd76c557b09797e2d02eea9db8ae3b8e
Shaun Rizk <shaunrizk@mail.com>
2020-05-28 22:32:40 -0600

    Implement bracket feature (#1)
    * Update README.md
    
    * Removed bundle js and fixed up broken stuff
    
    * Fixed up .gitignore
    
    * Added in a JS version of the application
    
    * Added in the participants component
    
    * Worked with Shaun to get the tournamentbracket over to javascript
    
    * Removed comments
    
    * updated some things
    
    * Got a basic thing working
    
    * Added in some of the boilerplate for deleting a participant
    
    * Moved everything around and added in some helper commands
    
    * Create Participants.jsx
    
    * moved into correct file
    
    * finalized delete command
    
    Co-authored-by: Daniel <danielzrust@gmail.com>

--- README.md
+++
@@ -1,8 +0,0 @@
-# Tournament-Bracket-Maker
-This will take a list of names and organize them into an orderly tournament.
-
-I wrote this code to be able to create an easy and quick tournament bracket for any type of game. The user can upload their pre-made text file, or just use the in built system to create the list of names they will use for the tournament. It takes the names into a list and creates another list that has the range of numbers from 1 to the length of the name list to be used as place spots in the tournament. It will then take these 2 lists and randomly assign a number to a name and place the combination into a dictionary called brackets. Then it will take this dictionary and order it into another dictionary from least to greatest key value because I want it to print out in order on the resulting page. In order to use the current code here, you make the Participants.txt with all the names of the people you want in the tournament, seperated by pressing the enter key.
-
-Future plans: I want to make the output page have boxes and the connecting lines that a normal bracket would look like.
-I also want to have the program to create a multiplayer battle situation instead of just 1 v 1 battle.
-Eventually I want to add in a GUI element and make it more user friendly, especially in adding the names to the tournament list.
---
+++ index.js
@@ -0,0 +1,75 @@
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
+const app = express()
+const port = 3000
+const bodyParser = require('body-parser')
+
+app.use('/public', express.static('dist'))
+const dbPath = path.join(__dirname, "./database.json");
+// parse application/json
+var jsonParser = bodyParser.json()
+app.use(bodyParser.json())
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"))
+}) // http://localhost:3000
+
+app.get('/participants-text', (req, res) => {
+    res.sendStatus(504);
+});
+
+// Browser -> http://localhost:3000/participants
+// Sever -> {} to client
+app.get('/participants', (req, res) => {
+    res.json(JSON.parse(fs.readFileSync(dbPath)));
+});
+
+app.post('/participants', (req, res) => {
+    const currentDatabase = JSON.parse(fs.readFileSync(dbPath));
+    const { participants } = currentDatabase;
+    currentDatabase.participants = [...participants, { ...req.body, id: participants[participants.length - 1].id + 1 }].sort(((a, b) => a.id - b.id));
+    fs.writeFileSync(dbPath, JSON.stringify(currentDatabase));
+    res.status(200).send();
+});
+
+app.put('/participants', (req, res) => {
+    const currentDatabase = JSON.parse(fs.readFileSync(dbPath));
+    const { participants } = currentDatabase;
+    currentDatabase.participants = [...participants.filter(p => p.id !== req.body.id), { ...req.body }].sort(((a, b) => a.id - b.id));
+    fs.writeFileSync(dbPath, JSON.stringify(currentDatabase));
+    res.status(200).send();
+});
+
+app.delete('/participants/:id', (req, res) => {
+    const currentDatabase = JSON.parse(fs.readFileSync(dbPath));
+    const { participants } = currentDatabase;
+    const { id } = req.params;
+    // Remove from array by id
+    currentDatabase.participants = participants.filter(p => p.id !== Number(id)).sort(((a, b) => a.id - b.id));
+    fs.writeFileSync(dbPath, JSON.stringify(currentDatabase));
+    res.status(200).send();
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+// MANA -- MongoDB, Apache, NodeJs, AngularJS
+// MANR -- MongoDB, Apache, NodeJs, ReactJS
+// SANR -- SQL, Apache, NodeJS, ReactJS
+
+// Teachers | Id, Name
+
+// Students | Id, Name, TeacherId
+
+// Mongo
+// student: {
+//     id: "3",
+//     name: "John Doe",
+//     Teacher: {
+//         id: "1",
+//         name: "Mr Rizk",
+//         Id: "32"
+//     }    
+// }
+
+//https://www.google.com
+// Domain Name Servers -> https://.... -> 8.8.8.8
